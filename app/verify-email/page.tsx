c1d1e14cde7b89183917c09c7351eca5529ffb10
Mo'men <mmm.isam.99@gmail.com>
2026-07-18 18:30:44 +0300

    deploay on vercel

--- app/verify-email/page.tsx
+++ app/verify-email/page.tsx
@@ -1,26 +1,9 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
-import { useEffect } from "react";
 
 const VerifyPage = () => {
-  const token = useSearchParams().get("token");
-  console.log("the token is", token);
-  useEffect(() => {
-    const verifyEmail = async () => {
-      const response = await fetch(
-        `http://localhost:5000/api/auth/verify-email?token=${token}`,
-      );
-      const data = await response.json();
-      console.log(data);
-      if (data.success) {
-        console.log("success");
-      } else {
-        console.log("error");
-      }
-    };
-    verifyEmail();
-  }, []);
+  console.log("the token is",);
+
   return <div>VerifyPage</div>;
 };
 
