65a522a605c8c277abf6a1d0aabdfc2978718762
Mo'men <mmm.isam.99@gmail.com>
2026-08-10 15:29:08 +0300

    refactor(lesson): integrate media into lesson details

--- app/(pages)/instructor-dashboard/lessons/[id]/details/page.tsx
+++ app/(pages)/instructor-dashboard/lessons/[id]/details/page.tsx
@@ -1,5 +1,6 @@
 import LessonDetails from "@/app/_modules/lesson/views/lesson-details";
-import { TParams } from "@/types/params";
+import { MediaDialogProvider } from "@/app/_modules/media/context/media-dialog-context";
+
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -7,14 +8,11 @@ export const metadata: Metadata = {
   description: "This is the lesson details page",
 };
 
-const LessonDetailsPage = async ({ params }: TParams) => {
-  const { id } = await params;
+const LessonDetailsPage = () => {
   return (
-    <div>
-      <LessonDetails
-        manageMedia={`/instructor-dashboard/lessons/${id}/media`}
-      />
-    </div>
+    <MediaDialogProvider>
+      <LessonDetails />
+    </MediaDialogProvider>
   );
 };
 export default LessonDetailsPage;
